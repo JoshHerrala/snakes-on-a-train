0af1c9a12297e6e5039cb3898899f0d316598819
JoshHerrala <josh@unionbayproject.com>
2021-10-10 22:30:55 -0400

    break point

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ text = "Welcome to Snakes on a Train, a Python parable. \n\nPlease select the wh
 options = "\nMath = M, Strings = S, Collections = C"
 print(text, options)
 
-user_input = input("Your selection")
+
+user_input = input("Your selection: ")
 print(user_input)
+
+math = "M"
+
+breakpoint()
+
+if user_input == math :
+  print("Its Math")
+else:
+  print("Its not Math")
 
 
 
