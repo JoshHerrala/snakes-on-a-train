bc79236992f4f9b190593997fea9d0e5655782e8
JoshHerrala <josh@unionbayproject.com>
2021-10-10 21:49:55 -0400

    user selection

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-text = "hello world"
-print(text)
+text = "Welcome to Snakes on a Train, a Python parable. \n\nPlease select the what you would like to do:"
+options = "\nMath = M, Strings = S, Collections = C"
+print(text, options)
+
+user_input = input("Your selection")
+print(user_input)
+
+
+
