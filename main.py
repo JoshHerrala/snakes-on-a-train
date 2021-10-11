371852ee19437fad07bc3e01cd0c2e9a264a9ee4
JoshHerrala <josh@unionbayproject.com>
2021-10-11 00:51:40 -0400

    zee boilerplate

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 text = "Welcome to Snakes on a Train, a Python parable. \n\nPlease select the what you would like to do:"
 options = "\nMath = M, Strings = S, Collections = C"
 print(text, options)
@@ -16,4 +17,10 @@ else:
   print("Its not Math")
 
 
-
+#TODO make functions above only executed on main.
+if __name__ == '__main__':
+  print("Function has been called as main")
+else:
+  print("Called from another function")
+    #functionA()
+    #functionB()
