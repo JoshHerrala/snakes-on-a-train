3f2a757ae50cbbc50a9c5e628a8fef5ddde622ea
JoshHerrala <josh@unionbayproject.com>
2021-10-11 20:46:52 -0400

    add main method

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 
-text = "Welcome to Snakes on a Train, a Python parable. \n\nPlease select the what you would like to do:"
-options = "\nMath = M, Strings = S, Collections = C"
-print(text, options)
+def main():
+  text = "Welcome to Snakes on a Train, a Python parable. \n\nPlease select the what you would like to do:"
+  options = "\nMath = M, Strings = S, Collections = C"
+  print(text, options)
+  user_input = input("Your selection: ")
+  print(user_input)
+  #breakpoint()
+  if user_input == "M" :
+    print("Its Math")
+  else:
+    print("Its not Math")
 
 
-user_input = input("Your selection: ")
-print(user_input)
-
-math = "M"
-
-breakpoint()
-
-if user_input == math :
-  print("Its Math")
-else:
-  print("Its not Math")
-
-
-#TODO make functions above only executed on main.
 if __name__ == '__main__':
+  main()
   print("Function has been called as main")
 else:
   print("Called from another function")
